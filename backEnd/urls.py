605be03147d4c0bf14e241382342387700863d97
Eclear <eclearning@outlook.com>
2018-06-27 15:06:34 +0800

    add index and register page

--- backEnd/urls.py
+++ backEnd/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     # url(r'^$',views.game_view,name='game_view'),
+    url(r'^$', views.online_page,name='online_page'),
+    url(r'^register/$', views.register_page,name='register_page'),
+    url(r'^registeraction/$', views.register,name='register'),
 ]
--- backEnd/views.py
+++ backEnd/views.py
@@ -3,6 +3,11 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+def online_page(request):
+    # return render(request, 'tem2/index.html')
+    return render(request,'online/index.html')
+
+
 
 # def game_view(request):
 #     data = {
@@ -10,3 +15,22 @@ from django.shortcuts import render
 #     }
 #     return render(request, 'tinyHeart/../game/templates/tinyHeart.html', data)
 #     # return render(request, '1.html', data)
+
+def register_page(request):
+    data = {
+        # 'type_list': ['manager','waiter','cook'],
+    }
+    return render(request,'online/register_page.html',data)
+
+def register(request):
+    _username = request.POST.get('username')
+    _password = request.POST.get('password')
+    _type = request.POST.get('type_')
+    user_list = User.objects.filter(username=_username)
+    #用户名是否存在
+    if(len(user_list)>0):
+        return HttpResponse('该用户名已存在！')
+    else:
+        new_user = User.objects.create(username=_username,password=_password,type=_type)
+        ov.CURRENT_ID = new_user.user_id
+        return HttpResponseRedirect(reverse('online:online_page'))
